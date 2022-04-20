0e77346ff2678db1298d7fc8155536fc9f828312
nicosposito <nicolas.sposito16@hotmail.com>
2022-04-20 19:04:21 -0300

    Fixes info peli, falta

--- src/components/InformacionPelicula.vue
+++ src/components/InformacionPelicula.vue
@@ -1,33 +1,34 @@
 <template>
   <h1>Información de pelicula</h1>
-  <v-card class="mx-auto">
+  <v-container>
     <v-card-title class="justify-center">
       {{ pelicula.nombre }}
     </v-card-title>
     <v-row>
-      <v-col md="6">
+      <v-col md="3">
         <div class="d-flex flex-no-wrap">
-          <v-avatar class="ma-3" size="500" rounded="0">
+          <v-avatar class="ma-3" size="400" rounded="0">
             <v-img :src="pelicula.imgSrc"></v-img>
           </v-avatar>
         </div>
       </v-col>
-      <v-col md="6">
+      <v-col md="4">
         <div class="d-flex flex-column">
-          <h1>Sinopsis</h1>
-          <label>{{ pelicula.sinopsis }}</label
+          <h1>Sinopsis:</h1>
+          <label id="sinopsis">{{ pelicula.sinopsis }}</label
           ><br />
-          <h1>Otros datos</h1>
-          <label>Director: {{ pelicula.director }}</label
+          <h1>Otros datos:</h1>
+          <label>Director: {{ pelicula.director }}.</label
           ><br />
-          <label>Genero: {{ pelicula.genero }}</label
+          <label>Genero: {{ pelicula.genero }}.</label
           ><br />
-          <label>Duración: {{ pelicula.duracion }}</label
-          ><br />
+          <label>Duración: {{ pelicula.duracion }} minutos.</label
+          >
+          <br />
         </div>
       </v-col>
     </v-row>
-  </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -55,5 +56,15 @@ h1 {
 
 .v-card {
   border: 1px solid black;
+}
+
+#sinopsis{
+  display: inline-block;
+  text-align: left;
+}
+
+.v-container {
+  border: 1px solid black;
+  padding: 30px;
 }
 </style>
